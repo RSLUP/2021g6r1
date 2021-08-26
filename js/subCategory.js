e03851be8fa6a85b1716abbfdf3abeb9d27ac97d
TharinduLiyanage <78396782+TharinduLiyanage@users.noreply.github.com>
2021-08-26 16:39:55 +0530

    error handled

---
+++ js/subCategory.js
@@ -0,0 +1,26 @@
+function sub1() {
+    var x = document.getElementById("subBox1");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function sub2() {
+    var x = document.getElementById("subBox2");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function sub3() {
+    var x = document.getElementById("subBox3");
+
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
