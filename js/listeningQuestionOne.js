cdf5f1178c7650044593a5901e18999d2a6f21ea
gayan <gayanwebdeveloper@gmail.com>
2021-09-04 22:28:59 +0530

    I have added listening test quiz

---
+++ js/listeningQuestionOne.js
@@ -0,0 +1,59 @@
+
+let questions = [
+    {
+    numb: 1,
+    question: "Why she cant't see out side of the window clearly?",
+    answer: "The morning rain clouds up my window",
+    options: [
+      "He drank too much last night",
+      "Drakness",
+      "window was very dirty",
+      "The morning rain clouds up my window"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Why she thank someone?",
+    answer: "To be with her",
+    options: [
+      "Handed her a towel",
+      "To be with her",
+      "Giving a present",
+      "None of above"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Why hear head just feels in pain?",
+    answer: "All of above",
+    options: [
+      "Missed the bus",
+      "Drank too much last night,",
+      "Got bills to pay",
+      "All of above"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What reminds her that's not bad?",
+    answer: "The picture on her wall",
+    options: [
+      "The cat on the table",
+      "The picture on her wall",
+      "Mother in the room",
+      "Son in the room"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What this song is about?",
+    answer: "Someone who always with her",
+    options: [
+      "Her cat",
+      "Her house",
+      "Someone who always with her",
+      "None of above"
+    ]
+  },
+    
+];
