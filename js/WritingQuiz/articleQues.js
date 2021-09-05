af2c3e90f3d70c8034f4c3ae9d7035f930776d4d
gayan <gayanwebdeveloper@gmail.com>
2021-09-05 19:27:42 +0530

    I have updated content in writing quiz

--- js/WritingQuiz/articleQues.js
+++ js/WritingQuiz/articleQues.js
@@ -2,45 +2,45 @@ let questions = [
     {
     numb: 1,
     question: "I want ____ apple from that basket.",
-    answer: "an",
+    answer: "An",
     options: [
-      "an",
-      "",
-      "",
-      ""
+      "An",
+      "A",
+      "The",
+      "None of above"
     ]
   },
     {
     numb: 2,
     question: " ____ church on the corner is progressive.",
-    answer: "The ",
+    answer: "The",
     options: [
-      "",
-      "",
-      "",
-      "The"
+      "A",
+      "An",
+      "The",
+      "None of above"
     ]
   },
     {
     numb: 3,
     question: "Miss Lin speaks ____ Chinese.",
-    answer: "no article needed",
+    answer: "None of above",
     options: [
-      "",
-      "no article needed",
-      "",
-      ""
+      "The",
+      "a",
+      "An",
+      "None of above"
     ]
   },
     {
     numb: 4,
     question: "I borrowed ____ pencil from your pile of pencils and pens.",
-    answer: "a ",
+    answer: "A",
     options: [
-      "a ",
-      "",
-      "",
-      ""
+      "A",
+      "An",
+      "The",
+      "None of above"
     ]
   },
     {
@@ -48,10 +48,10 @@ let questions = [
     question: "One of the students said, '____ professor is late today'",
     answer: "The",
     options: [
-      "",
-      "",
-      "",
-      "The"
+      "A",
+      "An",
+      "The",
+      "All of above"
     ]
   },
 ];
